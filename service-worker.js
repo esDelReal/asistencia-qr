e509f6c2b459f3c09522d180a2b3ca2b3c771020
Erick <38107158+esDelReal@users.noreply.github.com>
2026-09-19 23:31:14 -0600

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,64 +2,35 @@ const CACHE_NAME = "asistencia-qr-v1";
 
 const ARCHIVOS = [
   "./",
-  "./index.html",
-  "./manifest.json",
-  "https://unpkg.com/html5-qrcode"
+  "./index.html"
 ];
 
 self.addEventListener("install", event => {
 
   event.waitUntil(
-
     caches.open(CACHE_NAME)
-      .then(cache => {
-
-        return cache.addAll(
-          ARCHIVOS
-        );
-
-      })
-      .catch(error => {
-
-        console.log(
-          "Error guardando archivos:",
-          error
-        );
-
-      })
-
+      .then(cache => cache.addAll(ARCHIVOS))
   );
 
   self.skipWaiting();
-
 });
 
 
 self.addEventListener("activate", event => {
 
   event.waitUntil(
+    caches.keys().then(keys => {
 
-    caches.keys()
-      .then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
 
-        return Promise.all(
-
-          keys
-            .filter(
-              key => key !== CACHE_NAME
-            )
-            .map(
-              key => caches.delete(key)
-            )
-
-        );
-
-      })
-
+    })
   );
 
   self.clients.claim();
-
 });
 
 
@@ -67,59 +38,37 @@ self.addEventListener("fetch", event => {
 
   event.respondWith(
 
-    caches.match(event.request)
-      .then(cachedResponse => {
+    fetch(event.request)
+      .then(response => {
 
-        if (cachedResponse) {
+        if (
+          response &&
+          response.status === 200
+        ) {
 
-          return cachedResponse;
+          const copia =
+            response.clone();
+
+          caches.open(CACHE_NAME)
+            .then(cache => {
+
+              cache.put(
+                event.request,
+                copia
+              );
+
+            });
 
         }
 
-        return fetch(event.request)
-          .then(response => {
+        return response;
 
-            /*
-              Guardamos únicamente respuestas
-              válidas.
-            */
+      })
+      .catch(() => {
 
-            if (
-              response &&
-              response.status === 200 &&
-              response.type !== "opaque"
-            ) {
-
-              const responseClone =
-                response.clone();
-
-              caches.open(CACHE_NAME)
-                .then(cache => {
-
-                  cache.put(
-                    event.request,
-                    responseClone
-                  );
-
-                });
-
-            }
-
-            return response;
-
-          })
-          .catch(() => {
-
-            /*
-              Si no hay Internet y no existe
-              en caché, devolvemos la página.
-            */
-
-            return caches.match(
-              "./index.html"
-            );
-
-          });
+        return caches.match(
+          event.request
+        );
 
       })
 
